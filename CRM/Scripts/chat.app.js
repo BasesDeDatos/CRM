b04287523d82a544c1a8a5697de4cd25f3eb5863
Isaac Campos <kakoo26i@gmail.com>
2017-11-19 19:50:15 -0600

    Se agrega la funcionalidad del Chat

---
+++ CRM/Scripts/chat.app.js
@@ -0,0 +1,36 @@
+﻿var app = angular.module('chatApp', ['firebase']);
+
+app.controller('chatController', ['$scope', 'Message', function ($scope, Message) {
+
+    $scope.user = "Guest";
+
+    $scope.messages = Message.all;
+
+    $scope.inserisci = function (message) {
+        Message.create(message);
+    };
+}]);
+
+
+app.factory('Message', ['$firebaseObject', '$firebaseArray',
+	function ($firebaseObject, $firebaseArray) {
+	    var ref = firebase.database().ref();
+	    var messages = $firebaseArray(ref.child('messages'));
+ 
+		var Message = {
+			all: messages,
+			create: function (message) {
+				return messages.$add(message);
+			},
+			get: function (messageId) {
+			    return $firebaseObject(ref.child('messages').child(messageId));
+			},
+			delete: function (message) {
+				return messages.$remove(message);
+			}
+		};
+ 
+		return Message;
+ 
+	}
+	]);
